470d261cd275daa95608025b6c88b8447dcdeaf0
Alexander Buddenbrock <a.buddenbrock@ish.de>
2015-07-11 10:18:12 +0200

    Add team.hpp

---
+++ include/dota2api/team.hpp
@@ -0,0 +1,13 @@
+#ifndef TEAM_HPP_VBXTG91H
+#define TEAM_HPP_VBXTG91H
+
+namespace dota2
+{
+    enum class Team
+    {
+        RADIANT,
+        DIRE
+    };
+}
+
+#endif /* end of include guard: TEAM_HPP_VBXTG91H */
