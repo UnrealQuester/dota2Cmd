92df9595ec91385de32f9b2e0e0572422cf5897f
Alexander Buddenbrock <a.buddenbrock@ish.de>
2015-07-27 11:37:54 +0200

    Add case macro for better enum handling
    The case(int)Enum: return Enum; construct can be replaced by a macro to
    avoid repetition.

---
+++ include/dota2api/_util.hpp
@@ -0,0 +1,7 @@
+#ifndef _UTIL_HPP_XZFYA6SR
+#define _UTIL_HPP_XZFYA6SR
+
+#define enumCase(e) case (int)e: return e
+#define enumCaseDefault(e) default: return e
+
+#endif /* end of include guard: _UTIL_HPP_XZFYA6SR */
