9707945a58925bd6428e19188c2f903772dc3a24
Alexander Buddenbrock <a.buddenbrock@ish.de>
2015-07-28 12:58:17 +0200

    Use a map for gameModes
    Users now can iterate over every GameMode and gameModeFromInt() got
    simpler. _util.hpp is no longer needed.

--- include/dota2api/_util.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef _UTIL_HPP_XZFYA6SR
-#define _UTIL_HPP_XZFYA6SR
-
-#define enumCase(e) case (int)e: return e
-#define enumCaseDefault(e) default: return e
-
-#endif /* end of include guard: _UTIL_HPP_XZFYA6SR */
--- include/dota2api/gamemode.cpp
+++ include/dota2api/gamemode.cpp
@@ -1,32 +1,11 @@
 #include "gamemode.hpp"
-#include "_util.hpp"
-using namespace dota2;
-GameMode dota2::gameModeFromInt(int mode)
+
+dota2::GameMode dota2::gameModeFromInt(int mode)
 {
-    switch(mode)
+    const auto iter = gameModes.find(mode);
+    if(iter == gameModes.end())
     {
-        enumCase(GameMode::Unknwown);
-        enumCase(GameMode::All_Pick);
-        enumCase(GameMode::Captains_Mode);
-        enumCase(GameMode::Random_Draft);
-        enumCase(GameMode::Single_Draft);
-        enumCase(GameMode::All_Random);
-        enumCase(GameMode::Intro);
-        enumCase(GameMode::Diretide);
-        enumCase(GameMode::Reverse_Captains_Mode);
-        enumCase(GameMode::Greeviling);
-        enumCase(GameMode::Tutorial);
-        enumCase(GameMode::Mid_Only);
-        enumCase(GameMode::Least_Played);
-        enumCase(GameMode::New_Player_Pool);
-        enumCase(GameMode::Compendium_Matchmaking);
-        enumCase(GameMode::Custom);
-        enumCase(GameMode::Captains_Draft);
-        enumCase(GameMode::Balanced_Draft);
-        enumCase(GameMode::Ability_Draft);
-        enumCase(GameMode::Event);
-        enumCase(GameMode::All_Random_Death_Match);
-        enumCase(GameMode::Solo_Mid);
-        enumCaseDefault(GameMode::Unknwown);
+        return GameMode::Unknwown;
     }
+    return iter->second;
 }
