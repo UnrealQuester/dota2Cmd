289940f562c82de1b135894ce70f3c808485986d
Alexander Buddenbrock <a.buddenbrock@ish.de>
2015-10-18 08:58:08 +0200

    Split long strin ginto multiple lines

--- tests/apirequesttest.cpp
+++ tests/apirequesttest.cpp
@@ -30,7 +30,9 @@ TEST(APIRequest, Query)
     EXPECT_EQ
     (
         request.getUrl(),
-        "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V1/?key=key&hero_id=1234&name=asdf"
+        "https://api.steampowered.com/"
+        "IDOTA2Match_570/GetMatchHistory/"
+        "V1/?key=key&hero_id=1234&name=asdf"
     );
 }
 
@@ -44,7 +46,9 @@ TEST(APIRequest, Escaping)
     EXPECT_EQ
     (
         request.getUrl(),
-        "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V1/?key=key&player_name=foo%20bar"
+        "https://api.steampowered.com/"
+        "IDOTA2Match_570/GetMatchHistory/"
+        "V1/?key=key&player_name=foo%20bar"
     );
 }
 
